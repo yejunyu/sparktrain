b9eec560aa7b856e39ae9029a6fba450d991707c
YEJUNYU <YEJUNYU@kingsoft.com>
2018-07-24 17:16:21 +0800

    sparkSQL

---
+++ src/main/scala/com/yejunyu/sparkSQL/SQLContextApp.scala
@@ -0,0 +1,28 @@
+package com.yejunyu.sparkSQL
+
+import org.apache.spark.sql.SQLContext
+import org.apache.spark.{SparkConf, SparkContext}
+
+/**
+  * @author yejunyu
+  * @date 18-7-24. 
+  */
+object SQLContextApp {
+
+  def main(args: Array[String]): Unit = {
+    // 1. 创建context
+    val sparkConf = new SparkConf()
+    // 测试环境
+    sparkConf.setAppName("SQLContextAPP").setMaster("local[2]")
+    val sc = new SparkContext(sparkConf)
+    var sqlContext = new SQLContext(sc)
+
+    // 2 处理
+    val path = "src/main/scala/com/yejunyu/sparkSQL/example/people.json"
+    val people = sqlContext.read.format("json").load(path)
+    people.printSchema()
+    people.show()
+    // 3 关闭资源
+    sc.stop()
+  }
+}
