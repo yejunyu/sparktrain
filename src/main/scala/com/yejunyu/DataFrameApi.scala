e58e8465fd0b4158fb06eccc296f555e12f9ba58
YEJUNYU <YEJUNYU@kingsoft.com>
2018-07-25 17:23:25 +0800

    dataframe操作

--- src/main/scala/com/yejunyu/DataFrameApi.scala
+++ src/main/scala/com/yejunyu/DataFrameApi.scala
@@ -29,5 +29,27 @@ object DataFrameApi {
     // 按年龄分组显示
     empDF.groupBy("age").count().show()
     spark.stop()
+
+    /*
+    // 默认位true,过长的信息隐藏
+    studentDF.show(30, false)
+    studentDF.take(10)
+    studentDF.first()
+    studentDF.head(3)
+    studentDF.select("email").show(30,false)
+    studentDF.filter("name=''").show
+    studentDF.filter("name='' OR name='NULL'").show
+    //name以M开头的人
+    studentDF.filter("SUBSTR(name,0,1)='M'").show
+    studentDF.sort(studentDF("name")).show
+    studentDF.sort(studentDF("name").desc).show
+    studentDF.sort("name","id").show
+    studentDF.sort(studentDF("name").asc, studentDF("id").desc).show
+
+    studentDF.select(studentDF("name").as("student_name")).show
+    val studentDF2 = rdd.map(_.split("\\|")).map(line => Student(line(0).toInt, line(1), line(2), line(3))).toDF()
+
+    studentDF.join(studentDF2, studentDF.col("id") === studentDF2.col("id")).show
+    */
   }
 }
