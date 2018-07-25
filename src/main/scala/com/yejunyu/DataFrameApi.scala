a34fd60f49324039c754fc013eb3e14709fd8586
YEJUNYU <YEJUNYU@kingsoft.com>
2018-07-25 14:55:33 +0800

    dataFrame操作

--- src/main/scala/com/yejunyu/DataFrameApi.scala
+++ src/main/scala/com/yejunyu/DataFrameApi.scala
@@ -14,6 +14,20 @@ object DataFrameApi {
     val path = "hdfs://yejunyu-pc:8020/example/people.json"
     val empDF = spark.read.format("json").load(path)
     empDF.printSchema()
+    // 默认是20条记录
+    empDF.show(100)
+    // 查询某一列的数据
+    empDF.select("name").show()
+    empDF.select(empDF.col("name")).show()
+    // 查询某几列的数据并进行并行计算
+    empDF.select(empDF.col("name"), empDF.col("age") + 10).show()
+    empDF.select(empDF.col("name"),
+      empDF.col("age") + 10).as("age2").show()
+    // 条件查询
+    empDF.filter(empDF.col("age") > 19).show()
+    empDF.where(empDF.col("age") > 19).show()
+    // 按年龄分组显示
+    empDF.groupBy("age").count().show()
     spark.stop()
   }
 }
