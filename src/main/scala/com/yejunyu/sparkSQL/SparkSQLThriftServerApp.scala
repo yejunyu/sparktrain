1da652219a4bca1a3c5fe8a1e8d4229180ebc55a
YEJUNYU <YEJUNYU@kingsoft.com>
2018-07-25 11:16:05 +0800

    Thrift

--- src/main/scala/com/yejunyu/sparkSQL/SparkSQLThriftServerApp.scala
+++ src/main/scala/com/yejunyu/sparkSQL/SparkSQLThriftServerApp.scala
@@ -3,6 +3,9 @@ package com.yejunyu.sparkSQL
 import java.sql.DriverManager
 
 /**
+  * 1. 首先启动hadoop start-all.sh
+  * 2. ./start-thriftserver.sh --master local[2] --jars /usr/local/src/hive-1.1.0-cdh5.15.0/lib/mysql-connector-java-5.1.44.jar
+  *
   * @author yejunyu
   * @date 18-7-24. 
   */
